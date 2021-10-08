a9294f45ff458daf43a50774188504028c6b954e
Diego Ar <91506473+roandiego@users.noreply.github.com>
2021-10-07 20:46:57 -0500

    Tratamos de mejorar un poco
    Valor Absoluto con Grafica :)

---
+++ funcion .m
@@ -0,0 +1,18 @@
+% Octave Script
+% Title			:Valor Absoluto
+% Description		:Funcion de V. Abs.
+% Author		:Diego Romero Antonio
+% Date			:202123778
+% Version		:1
+% Usage			:C:\WINDOWS\system32
+% Notes			:Requiere aplicacion octave usar en consola preferentemente
+
+"Funtion in Valor Absoluto"
+
+z = 10 - 20i
+conj(z)
+abs(z)
+z * conj(z)
+abs(z) ^ 20
+sqrt(14 + 22)
+axis (1 - 20i)
