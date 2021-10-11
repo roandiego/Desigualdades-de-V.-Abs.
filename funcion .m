72fd6c2e07c9765641fd9e6bc0ff42a193b5aa14
magalivalencia <91564173+magalivalencia@users.noreply.github.com>
2021-10-11 16:31:13 -0500

    actualización de inecuaciones 1y 2 grado
    actualización de inecuaciones

--- funcion .m
+++ funcion .m
@@ -1,11 +1,37 @@
 % Octave Script
 % Title			:Valor Absoluto
 % Description		:Funcion de V. Abs.
-% Author		:Diego Romero Antonio
+% Author		:Diego, Magali, Bradley
 % Date			:202123778
 % Version		:1
 % Usage			:C:\WINDOWS\system32
 % Notes			:Requiere aplicacion octave usar en consola preferentemente
+
+
+% Inecuaciones de primer grado
+pkg load symbolic 
+syms x
+disp('----------------inecuaciones de primer grado--------');
+disp('---3x+12>0---');
+solve(3x>-12/3)
+
+syms x
+f=3*X+12/3
+subplot (3,3,2)
+ezpot (f)
+
+% Inecuaciones de segundo grado
+pkg load symbolic
+syms x
+disp('-----------------inecuaciones de segundo grado-------');
+disp('----5x^2+3x+8<0---')
+
+solve (5x^2+3x-8)
+
+syms x
+f=(5x^2*2+3x-8
+subplot (3,3,2)
+ezplot (f)
 
 "Funtion in Valor Absoluto"
 
